e99d052e42d0e6cb41c5182e541ce7f2839c6de1
DasaniH2O <83208826+DasaniH2O@users.noreply.github.com>
2024-10-18 14:28:33 -0400

    Update custom-voiceglow.js

--- custom-voiceglow.js
+++ custom-voiceglow.js
@@ -1,6 +1,25 @@
 (function () {
+  var chatBubble = document.getElementById("yourID");
+  var customID = chatBubble.dataset.vgId;
+
+  var overlayContainer = document.createElement("div");
+  overlayContainer.id = "VG_OVERLAY_CONTAINER";
+  overlayContainer.innerHTML = "<!-- Chat widget will be rendered here -->";
+
+  var chatBubble = document.createElement("div");
+  chatBubble.id = "custom-chat-bubble";
+  chatBubble.innerHTML = `
+    <div class="ai-image"></div>
+    <span class="vg-bubble-text">
+      Need help? Want to send us feedback?
+      <span class="click-text"> Click here.</span>
+    </span>`;
+
+  document.body.appendChild(overlayContainer);
+  document.body.appendChild(chatBubble);
+
   window.VG_CONFIG = {
-    ID: "2thrnezcsbhwtjmu",
+    ID: customID,
     region: "na",
     render: "full-width",
     stylesheets: ["https://vg-bunny-cdn.b-cdn.net/vg_live_build/styles.css"],
@@ -9,22 +28,16 @@
   VG_SCRIPT.src = "https://vg-bunny-cdn.b-cdn.net/vg_live_build/vg_bundle.js";
   VG_SCRIPT.defer = true;
   document.body.appendChild(VG_SCRIPT);
-})();
 
-let active = false;
-
-document
-  .getElementById("custom-chat-bubble")
-  .addEventListener("click", function () {
+  let active = false;
+  chatBubble.addEventListener("click", function () {
     console.log("Hello");
     if (!active) {
-      document.getElementById("VG_OVERLAY_CONTAINER").classList.add("active");
+      overlayContainer.classList.add("active");
       active = true;
     } else {
-      document
-        .getElementById("VG_OVERLAY_CONTAINER")
-        .classList.remove("active");
+      overlayContainer.classList.remove("active");
       active = false;
     }
   });
-
+})();
